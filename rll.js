6fd07704325b06f4a11f653a1c36e7872ea2dcaf
bucho <steelwind666@gmail.com>
2015-08-19 20:55:03 +0900

    名前空間実装 modified:   rll.js

--- rll.js
+++ rll.js
@@ -1,59 +1,60 @@
+var rll = rll || {};
 
-var Point = function(x, y) {
+rll.Point = function(x, y) {
   this._x = x;
   this._y = y;
 };
 
-Point.prototype.x = function() {
+rll.Point.prototype.x = function() {
   return this._x;
 };
 
-Point.prototype.y = function() {
+rll.Point.prototype.y = function() {
   return this._y;
 };
 
-Point.prototype.add = function(other) {
+rll.Point.prototype.add = function(other) {
   var x = this.x() + other.x();
   var y = this.y() + other.y();
-  return new Point(x, y);
+  return new rll.Point(x, y);
 };
 
-Point.prototype.toString = function() {
+rll.Point.prototype.toString = function() {
   return this.x() + ':' + this.y();
 };
 
-Point.RIGHT = new Point(1, 0);
+rll.Point.RIGHT = new rll.Point(1, 0);
 
-var Font = function(family, size) {
+rll.Font = function(family, size) {
   this._fontFamily = family;
   this._fontSize = size;
 };
 
-Font.prototype.toString = function() {
+rll.Font.prototype.toString = function() {
   return this._fontSize + 'px ' + this._fontFamily;
 };
 
-Font.prototype.size = function() {
+rll.Font.prototype.size = function() {
   return this._fontSize;
 };
 
-var Size = function(width, height) {
+rll.Size = function(width, height) {
   this._width = width;
   this._height = height;
 };
 
-Size.prototype.width = function() {
+rll.Size.prototype.width = function() {
   return this._width;
 };
 
-Size.prototype.height = function() {
+rll.Size.prototype.height = function() {
   return this._height;
 };
 
-Size.prototype.multiply = function(other) {
+rll.Size.prototype.multiply = function(other) {
   var w = this.width()  * other.width();
   var h = this.height() * other.height();
-  return new Size(w, h);
+  return new rll.Size(w, h);
 };
 
 var Character = function(glyph, color) {
@@ -74,42 +75,42 @@ Character.prototype.equal = function(other) {
           this._color == other._color);
 };
 
-var Grid = function(point, ch) {
+rll.Grid = function(point, ch) {
   this._point = point;
   this._character = ch;
 };
 
-Grid._size = null;
-Grid._backGroundColor = '#000';
+rll.Grid._size = null;
+rll.Grid._backGroundColor = '#000';
 
-Grid.setSize = function(newSize) {
-  Grid._size = newSize;
+rll.Grid.setSize = function(newSize) {
+  rll.Grid._size = newSize;
 };
 
-Grid.prototype.equal = function(other) {
+rll.Grid.prototype.equal = function(other) {
   return this._character.equal(other._character);
 };
 
-Grid.prototype.draw = function(context) {
-  var w = Grid._size.width();
-  var h = Grid._size.height();
+rll.Grid.prototype.draw = function(context) {
+  var w = rll.Grid._size.width();
+  var h = rll.Grid._size.height();
   var x = this._point.x() * w;
   var y = this._point.y() * h;
-  context.fillStyle = Grid._backGroundColor;
+  context.fillStyle = rll.Grid._backGroundColor;
   context.fillRect(x, y, w, h);
   context.fillStyle = this._character.color();
   context.fillText(this._character.glyph(), x, y);
 };
 
-var Display = function() {
+rll.Display = function() {
   this._context = null;
-  this._font = new Font('Courier New', 15);
-  this._size = new Size(80, 25);
+  this._font = new rll.Font('Courier New', 15);
+  this._size = new rll.Size(80, 25);
   this._grids = {};
   this._dirty = {};
 };
 
-Display.prototype.initialize = function() {
+rll.Display.prototype.initialize = function() {
   var canvas = document.createElement('canvas');
   this._context = canvas.getContext('2d');
   this._setupFont();
@@ -117,45 +118,45 @@ Display.prototype.initialize = function() {
   this.clear();
 };
 
-Display.prototype._setupFont = function() {
+rll.Display.prototype._setupFont = function() {
   this._context.font = this._font.toString();
   this._context.textAlign    = 'left';
   this._context.textBaseline = 'top';
 };
 
-Display.prototype._computeCanvasSize = function() {
+rll.Display.prototype._computeCanvasSize = function() {
   var charWidth = Math.ceil(this._context.measureText('W').width);
-  var gridSize = new Size(charWidth, this._font.size());
+  var gridSize = new rll.Size(charWidth, this._font.size());
   var canvasSize = this._size.multiply(gridSize);
-  Grid.setSize(gridSize);
+  rll.Grid.setSize(gridSize);
   this._context.canvas.width = canvasSize.width();
   this._context.canvas.height = canvasSize.height();
   this._setupFont();
 };
 
-Display.prototype.getCanvas = function() {
+rll.Display.prototype.getCanvas = function() {
   return this._context.canvas;
 };
 
-Display.prototype.write = function(point, string, color) {
+rll.Display.prototype.write = function(point, string, color) {
   var write_color = color || '#ccc';
   // TODO 2byte文字対応
   for (var i=0; i < string.length; i++) {
     this._write(point, string[i], write_color);
-    point = point.add(Point.RIGHT);
+    point = point.add(rll.Point.RIGHT);
   }
 };
 
-Display.prototype._write = function(point, glyph, color) {
+rll.Display.prototype._write = function(point, glyph, color) {
   var key = point.toString();
-  var grid = new Grid(point, new Character(glyph, color));
+  var grid = new rll.Grid(point, new Character(glyph, color));
   if (key in this._dirty && this._dirty[key].equal(grid)) {
     return;
   }
   this._dirty[key] = grid;
 };
 
-Display.prototype.flush = function() {
+rll.Display.prototype.flush = function() {
   for (var key in this._dirty) {
     if (key in this._grids && this._grids[key].equal(this._dirty[key])) {
       continue;
@@ -166,28 +167,28 @@ Display.prototype.flush = function() {
   this._dirty = {};
 };
 
-Display.prototype.clear = function() {
+rll.Display.prototype.clear = function() {
   var w = this._context.canvas.width;
   var h = this._context.canvas.height;
-  this._context.fillStyle = Grid._backGroundColor;
+  this._context.fillStyle = rll.Grid._backGroundColor;
   this._context.fillRect(0, 0, w, h);
 };
 
-var Actor = function(point) {
+rll.Actor = function(point) {
   this._point = point;
 };
 
-Actor.prototype.draw = function(display) {
+rll.Actor.prototype.draw = function(display) {
   display.write(this._point, '@', '#088');
 };
 
-Actor.prototype.move = function(direction) {
+rll.Actor.prototype.move = function(direction) {
   this._point = this._point.add(direction);
 };
 
 var App = {
-  _display: new Display(),
-  _player: new Actor(new Point(1, 2)),
+  _display: new rll.Display(),
+  _player: new rll.Actor(new rll.Point(1, 2)),
 
   run: function() {
     this._display.initialize();
@@ -200,23 +201,23 @@ var App = {
   drawMap: function() {
     for (var x=0; x < 10; x++) {
       for (var y=0; y < 10; y++) {
-        this._display.write(new Point(x, y), '.');
+        this._display.write(new rll.Point(x, y), '.');
       }
     }
-    this._display.write(new Point(0, 0), 'hello 世界', '#0c0');
+    this._display.write(new rll.Point(0, 0), 'hello 世界', '#0c0');
     this._player.draw(this._display);
   },
 
   handleEvent: function(e) {
     var key = String.fromCharCode(e.keyCode).toLowerCase();
     if (key == 'h') {
-      this._player.move(new Point(-1, 0));
+      this._player.move(new rll.Point(-1, 0));
     } else if (key == 'l') {
-      this._player.move(new Point( 1, 0));
+      this._player.move(new rll.Point( 1, 0));
     } else if (key == 'k') {
-      this._player.move(new Point( 0,-1));
+      this._player.move(new rll.Point( 0,-1));
     } else if (key == 'j') {
-      this._player.move(new Point( 0, 1));
+      this._player.move(new rll.Point( 0, 1));
     } else {
       return;
     }
