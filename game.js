21aece49d62cbaf57d6449747b1dcfa9cdf9200e
bucho <steelwind666@gmail.com>
2015-09-11 21:42:38 +0900

    ai.jsの内容をgameに移動 deleted:    ai.js modified:   game.js modified:   index.html

--- game.js
+++ game.js
@@ -3,6 +3,79 @@ var game = game || {};
 
 game.keyEvent = new rll.KeyEvent();
 
+game.AI = function(game) {
+  this._game = game;
+  this._lastDest = null;
+};
+
+game.AI.prototype.compute = function(actor) {
+  var player = this._game.player();
+  var playerPoint = player.point();
+  if (this._lastDest && actor.on(this._lastDest)) {
+    this._lastDest = null;
+  }
+  if (actor.isNextTo(playerPoint) && player.isAlive()) {
+    this.attackToPlayer(actor, player);
+    return;
+  }
+  if (this.canSee(actor, playerPoint)) {
+    this._lastDest = playerPoint;
+    this.chase(actor, playerPoint);
+  } else if (this._lastDest) {
+    this.chase(actor, this._lastDest);
+  } else {
+    this.randomMove(actor);
+  }
+};
+
+game.AI.prototype.attackToPlayer = function(actor, player) {
+  var attack = new game.MeleeAttack(actor, player); // TODO game
+  if (attack.isHit() === false) {
+    this._game.message(actor.name() + 'の攻撃をかわした!');
+    return;
+  }
+  var damage = attack.damage();
+  this._game.message(actor.name() + 'の攻撃が命中' + damage + 'のダメージ!!');
+  if (player.isDead() === false) return;
+  this._game.message('あなたは死んだ…');
+};
+
+game.AI.prototype.canSee = function(actor, point) {
+  var stage = this._game.stage();
+  var line = actor.lineTo(point);
+  for (var i=0; i<line.length; i++) {
+    if (stage.passLight(line[i]) === false) {
+      return false;
+    }
+  }
+  return true;
+};
+
+game.AI.prototype.chase = function(actor, point) {
+  var stage = this._game.stage();
+  var directions = actor.directionsTo(point);
+  for (var i=0; i<directions.length; i++) {
+    var dir = directions[i];
+    var to = actor.movedPoint(dir);
+    if (stage.walkableAt(to)) {
+      actor.move(dir);
+      return;
+    }
+  }
+};
+
+game.AI.prototype.randomMove = function(actor) {
+  var stage = this._game.stage();
+  var directions = new rll.List();
+  for (var i=0; i<rll.Direction.AROUND.length; i++) {
+    var dir = rll.Direction.AROUND[i];
+    if (stage.walkableAt(actor.movedPoint(dir))) {
+      directions.push(dir);
+    }
+  }
+  actor.move(directions.choice());
+};
+
 game.Game = function() {
   this._display = new rll.Display();
   this._player  = new rll.Player(new rll.Character('@', '#fff'), 'player');
@@ -107,7 +180,7 @@ game.Game.prototype.newLevel = function() {
       });
     }
     m.setPoint(this._stage.randomWalkablePoint());
-    m.setAction(new rll.AI(this));
+    m.setAction(new game.AI(this));
     this._stage.addActor(m);
   }
 };
