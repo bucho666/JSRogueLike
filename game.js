80b593a91d12781abc5b21a31b24960482daff91
bucho <steelwind666@gmail.com>
2015-09-26 21:16:32 +0900

    戦闘処理を微調整 modified:   game.js

--- game.js
+++ game.js
@@ -378,7 +378,7 @@ game.MeleeAttack.prototype.isHit = function() {
   var rolls = rll.random(1, 20);
   if (rolls === 20) return true;
   if (rolls === 1 ) return false;
-  return rolls < (this._defender.armorClass() + this._attaker.toHit());
+  return rolls <= (this._defender.armorClass() + this._attaker.toHit());
 };
 
 game.MeleeAttack.prototype.damage = function() {
