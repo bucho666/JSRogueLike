74748e2d1b1d5e2a454225b673806d6ca6da4fe2
bucho <steelwind666@gmail.com>
2015-10-05 06:32:46 +0900

    リファクタリング modified:   index.html modified:   rll/actor.js new file:   rll/list.js

--- rll/actor.js
+++ rll/actor.js
@@ -176,65 +176,6 @@ rll.Humanoid.prototype.openableDoor = function() {
   return true;
 };
 
-rll.ChooseList = function(limit) { // TODO 別ファイル化
-  this._limit = limit;
-  this._items = [];
-  this._cursor = 0;
-};
-
-rll.ChooseList.prototype.draw = function(display) {
-  var backgroundColor;
-  for (var y=0; y<this._items.length; y++) {
-    backgroundColor = this._cursor === y ? '#080' : '#000';
-    display.write(new rll.Point(0, y), this.name(y), '#fff', backgroundColor);
-  }
-};
-
-rll.ChooseList.prototype.add = function(newItem) {
-  this._items.push(newItem);
-};
-
-rll.ChooseList.prototype.isEmpty = function() {
-  return this._items.isEmpty();
-};
-
-rll.ChooseList.prototype.isFull = function() {
-  return this._items.length >= this._limit;
-};
-
-rll.ChooseList.prototype.nextCursor = function() {
-  this._cursor += 1;
-  this._adjustCursor();
-};
-
-rll.ChooseList.prototype.prevCursor = function() {
-  this._cursor -= 1;
-  this._adjustCursor();
-};
-
-rll.ChooseList.prototype._adjustCursor = function() {
-  if (this._items.isEmpty()) {
-    this._cursor = 0;
-  } else {
-    this._cursor = (this._cursor + this._items.length) % this._items.length;
-  }
-};
-
-rll.ChooseList.prototype.name = function(index) {
-  return this._items[index].name();
-};
-
-rll.ChooseList.prototype.currentItem = function() {
-  return this._items[this._cursor];
-};
-
-rll.ChooseList.prototype.removeCurrentItem = function() {
-  var item = this._items[this._cursor];
-  this._items.splice(this._cursor, 1);
-  this._adjustCursor();
-  return item;
-};
-
 rll.ItemList = function(limit) {
   rll.ChooseList.call(this, limit);
   this._weapon = null;
